75d6b36df529f79ecdd2c286865cf9d464cf8fa1
bigboss98 <bigboss98@pop-os.localdomain>
2021-05-13 21:19:34 +0200

    Add Command Line arguments and BERT model

---
+++ src/fake_news_detection.py
@@ -0,0 +1,41 @@
+"""
+    Fake News detection using several ML models (BERT, LSTM, CNN and other)
+"""
+#import tensorflow as tf 
+import argparse 
+import pandas as pd 
+import json 
+import tensorflow as tf 
+from sklearn.model_selection import train_test_split
+from models import BERT 
+
+def command_line_arguments():
+
+    parser = argparse.ArgumentParser(description='Fake News detection using some ML models')
+    parser.add_argument('-training', metavar='TRAIN_PATH', type=str, default='../data/train_set.csv',
+                        help='Name of Training dataset (Default data/train_set.csv)', dest='training_path')
+    parser.add_argument('-test', default='../data/test_set.csv',
+                        dest='test_path', help='Name of Test dataset (Default data/test_set.csv)')
+    parser.add_argument('-models', type=str, default='default_models.json', 
+                        help='JSON file with models and their parameters to use to detect Fake News', dest='models')
+    #parser.add_argument('-parameters')
+    return parser.parse_args()
+    
+def initialize_models(models_config):
+    return BERT() 
+
+def fake_news_detection(command_line_args):
+    train_data = pd.read_csv(command_line_args.training_path)[["text", "label"]]
+    train_data, validation_data = train_test_split(train_data, test_size = 0.25)
+    test_data = pd.read_csv(command_line_args.test_path)[["text", "label"]]
+
+    
+    models = initialize_models(command_line_args.models)
+    models.fit((train_data["text"], train_data["label"]),
+               validation_data,
+                20)
+    print(models.evaluate(validation_data))
+
+if __name__== "__main__":
+    args = command_line_arguments()
+    fake_news_detection(args)
---
+++ src/models.py
@@ -0,0 +1,64 @@
+"""
+    Models used in Fake News Detection
+"""
+import os
+import shutil
+from typing import Dict 
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+from official.nlp import optimization  # to create AdamW optimizer
+
+import matplotlib.pyplot as plt
+
+tfhub_handle_encoder = 'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-128_A-2/1'
+tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
+
+class BERT():
+    def __init__(self, loss=tf.keras.losses.BinaryCrossentropy(),
+                 metrics=tf.metrics.BinaryAccuracy(), compile_model=True):
+        self.model = self.build_model()
+        self.loss = loss
+        self.metrics = metrics 
+        self.optimizer = self.build_optimizer()
+
+        if compile_model:
+            self.model.compile(optimizer=self.optimizer,
+                               loss=self.loss,
+                                metrics=self.metrics)
+
+    def build_model(self):
+        text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+        preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
+        encoder_inputs = preprocessing_layer(text_input)
+        encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
+        outputs = encoder(encoder_inputs)
+        net = outputs['pooled_output']
+        net = tf.keras.layers.Dropout(0.1)(net)
+        net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
+        return tf.keras.Model(text_input, net)
+
+    def build_optimizer(self):
+        epochs = 5
+        #steps_per_epoch = tf.data.experimental.cardinality(train_ds).numpy()
+        num_train_steps = 1 * epochs
+        num_warmup_steps = int(0.1*num_train_steps)
+
+        init_lr = 3e-5
+        return optimization.create_optimizer(init_lr=init_lr,
+                                             num_train_steps=num_train_steps,
+                                             num_warmup_steps=num_warmup_steps,
+                                             optimizer_type='adamw')
+
+
+    def fit(self, training, validation, epochs=100):
+        return self.model.fit(x=list(training[0]),
+                              y=list(training[1]),
+                              validation_data=validation,
+                              epochs=epochs)
+
+    def evaluate_model(self, test_set):
+        return self.model.evaluate(test_set)
+
+    def get_config(self) -> Dict:
+        pass 
