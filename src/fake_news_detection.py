5536029d3b9cae559998767d577df2aa18334d41
bigboss98 <bigboss98@pop-os.localdomain>
2021-05-14 00:57:42 +0200

    Add more epochs for fine tuning of BERT parameters

--- src/fake_news_detection.py
+++ src/fake_news_detection.py
@@ -1,37 +1,51 @@
 """
     Fake News detection using several ML models (BERT, LSTM, CNN and other)
 """
-#import tensorflow as tf 
-import argparse 
-import pandas as pd 
-import json 
-import tensorflow as tf 
-from sklearn.model_selection import train_test_split
-from models import BERT 
+import argparse
+import json
+import pandas as pd
+from models import BERT, loss_function, metric_function
 
 def command_line_arguments():
 
     parser = argparse.ArgumentParser(description='Fake News detection using some ML models')
-    parser.add_argument('-training', metavar='TRAIN_PATH', type=str, default='../data/train_set.csv',
-                        help='Name of Training dataset (Default data/train_set.csv)', dest='training_path')
+    parser.add_argument('-training', metavar='TRAIN_PATH', type=str,
+                        default='../data/train_set.csv',
+                        help='Name of Training dataset (Default data/train_set.csv)',
+                        dest='training_path')
     parser.add_argument('-test', default='../data/test_set.csv',
                         dest='test_path', help='Name of Test dataset (Default data/test_set.csv)')
-    parser.add_argument('-models', type=str, default='default_models.json', 
-                        help='JSON file with models and their parameters to use to detect Fake News', dest='models')
+    parser.add_argument('-models', type=str, default='../default_models.json',
+                        help='JSON file with models and their parameters to detect Fake News',
+                        dest='models')
     #parser.add_argument('-parameters')
     return parser.parse_args()
-    
+
 def initialize_models(models_config):
-    return BERT() 
+    models = []
+    with open(models_config) as models_file:
+        data = json.load(models_file)
+        if data["BERT"]:
+            models.append(initialize_bert(data["BERT"]))
+    return models
+
+def initialize_bert(bert_config):
+    loss = loss_function[bert_config["loss"]]
+    metrics = metric_function[bert_config["metrics"]]
+    epochs = bert_config["epochs"]
+    bert_encoder = bert_config["encoder_hub"]
+    bert_preprocessing = bert_config["preprocessing_hub"]
+    prob_dropout = bert_config["prob_dropout"]
+    return BERT(loss, metrics, epochs, bert_encoder, bert_preprocessing, prob_dropout)
 
 def fake_news_detection(command_line_args):
-    train_data = pd.read_csv(command_line_args.training_path)[["text", "label"]][:100]
-    #test_data = pd.read_csv(command_line_args.test_path)[["text", "label"]][:500]
+    train_data = pd.read_csv(command_line_args.training_path)[["text", "label"]][:1000]
+    test_data = pd.read_csv(command_line_args.test_path)[["text", "label"]][:500]
 
     models = initialize_models(command_line_args.models)
-    models.fit(train_data["text"], train_data["label"], 20)
-    #print(models.evaluate_model(test_data["text"], test_data["label"]))
+    models[0].fit(train_data["text"], train_data["label"])
+    print(models[0].evaluate_model(test_data["text"], test_data["label"]))
 
-if __name__== "__main__":
+if __name__ =="__main__":
     args = command_line_arguments()
     fake_news_detection(args)
--- src/models.py
+++ src/models.py
@@ -1,47 +1,47 @@
 """
     Models used in Fake News Detection
 """
-import os
-import shutil
-from typing import Dict 
+
 import tensorflow as tf
 import tensorflow_hub as hub
-import tensorflow_text as text
+import tensorflow_text 
 from official.nlp import optimization  # to create AdamW optimizer
 
 import matplotlib.pyplot as plt
 
-tfhub_handle_encoder = 'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-128_A-2/1'
-tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
+TFHUB_HANDLE_ENCODER = 'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-128_A-2/1'
+TFHUB_HANDLE_PREPROCESS = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
 
 class BERT():
     def __init__(self, loss=tf.keras.losses.BinaryCrossentropy(),
-                 metrics=tf.metrics.BinaryAccuracy(), compile_model=True):
-        self.model = self.build_model()
+                 metrics=tf.metrics.BinaryAccuracy(), epochs=50,
+                 hub_bert_encoder=TFHUB_HANDLE_ENCODER,
+                 hub_bert_preprocess=TFHUB_HANDLE_PREPROCESS, prob_dropout=0.1):
+        self.model = self.build_model(prob_dropout, hub_bert_preprocess, hub_bert_encoder)
         self.loss = loss
-        self.metrics = metrics 
+        self.metrics = metrics
         self.optimizer = self.build_optimizer()
+        self.epochs = epochs
 
-        if compile_model:
-            self.model.compile(optimizer=self.optimizer,
-                               loss=self.loss,
-                                metrics=self.metrics)
+        self.model.compile(optimizer=self.optimizer,
+                           loss=self.loss,
+                           metrics=self.metrics)
 
-    def build_model(self):
+    def build_model(self, prob_dropout, bert_preprocess, bert_encoder):
         text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-        preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
+        preprocessing_layer = hub.KerasLayer(bert_preprocess, name='preprocessing')
         encoder_inputs = preprocessing_layer(text_input)
-        encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
+        encoder = hub.KerasLayer(bert_encoder, trainable=True, name='BERT_encoder')
         outputs = encoder(encoder_inputs)
         net = outputs['pooled_output']
-        net = tf.keras.layers.Dropout(0.1)(net)
+        net = tf.keras.layers.Dropout(prob_dropout)(net)
         net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
         return tf.keras.Model(text_input, net)
 
     def build_optimizer(self):
-        epochs = 5
+        epochs = 50
         #steps_per_epoch = tf.data.experimental.cardinality(train_ds).numpy()
-        num_train_steps = 1 * epochs
+        num_train_steps = 10 * epochs
         num_warmup_steps = int(0.1*num_train_steps)
 
         init_lr = 3e-5
@@ -51,14 +51,19 @@ class BERT():
                                              optimizer_type='adamw')
 
 
-    def fit(self, training_text, training_label, epochs=100):
+    def fit(self, training_text, training_label):
         return self.model.fit(training_text,
                               training_label,
                               validation_split=0.25,
-                              epochs=epochs)
+                              epochs=self.epochs)
 
     def evaluate_model(self, test_text, test_label):
         return self.model.evaluate(test_text, test_label)
 
-    def get_config(self) -> Dict:
-        pass 
+loss_function = {
+    'binary_crossentropy': tf.keras.losses.BinaryCrossentropy(),
+}
+
+metric_function = {
+    'binary_accuracy': tf.metrics.BinaryAccuracy(),
+}
